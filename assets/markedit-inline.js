35616dcc3d83f9492c0e0c3bc71d946e1f21bb54
diversen <dennis.iversen@gmail.com>
2011-07-14 15:51:55 +0200

    added option for choosing an editor.

---
+++ assets/markedit-inline.js
@@ -0,0 +1,7 @@
+$(function(){
+        // Initialize MarkEdit with a custom toolbar layout and preview below the editor...
+    $('#markdown').markedit({
+        'preview' : 'bottom',
+        
+    });
+});
